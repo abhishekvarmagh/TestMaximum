6220285675e9b89693ce69e9101fab5a02827963
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2020-02-28 17:42:22 +0530

    REFACTORED : All Three Method To One Generic Method

--- src/main/java/com/generic/FindMaximumNumber.java
+++ src/main/java/com/generic/FindMaximumNumber.java
@@ -2,30 +2,12 @@ package com.generic;
 
 public class FindMaximumNumber {
 
-    public Integer largest(Integer firstNumber, Integer secondNumber, Integer thirdNumber){
-        Integer max = firstNumber;
+    public <E extends Comparable<E>> E largest(E firstNumber, E secondNumber, E thirdNumber) {
+        E max = firstNumber;
         if (secondNumber.compareTo(max) > 0)
             max = secondNumber;
         if (thirdNumber.compareTo(max) > 0)
             max = thirdNumber;
-        return max;
-    }
-
-    public Float largest(Float firstNumber, Float secondNumber, Float thirdNumber){
-        Float max = firstNumber;
-        if (secondNumber.compareTo(max) > 0)
-            max = secondNumber;
-        if (thirdNumber.compareTo(max) > 0)
-            max = thirdNumber;
-        return max;
-    }
-
-    public String largest(String x, String y, String z){
-        String max = x;
-        if (y.compareTo(max) > 0)
-            max = y;
-        if (z.compareTo(max) > 0)
-            max = z;
         return max;
     }
 }
--- src/test/java/com/generic/TestMaximumNumber.java
+++ src/test/java/com/generic/TestMaximumNumber.java
@@ -16,26 +16,26 @@ public class TestMaximumNumber {
 
     @Test
     public void givenMaxNumber_AtFirstPosition_ShouldReturnSameNumber() {
-        int largest = max.largest(3,2,1);
+        int largest = max.largest(3, 2, 1);
         Assert.assertEquals(3, largest);
     }
 
     @Test
     public void givenMaxNumber_AtSecondPosition_ShouldReturnSameNumber() {
-        int largest = max.largest(2,3,1);
+        int largest = max.largest(2, 3, 1);
         Assert.assertEquals(3, largest);
     }
 
     @Test
     public void givenMaxNumber_AtThirdPosition_ShouldReturnSameNumber() {
-        int largest = max.largest(2,1,3);
+        int largest = max.largest(2, 1, 3);
         Assert.assertEquals(3, largest);
     }
 
     @Test
     public void givenMaxFloatNumber_AtFirstPosition_ShouldReturnSameNumber() {
         float largest = max.largest(3.0f, 2.0f, 1.0f);
-        Assert.assertEquals(3.0f, largest,0.0f );
+        Assert.assertEquals(3.0f, largest, 0.0f);
     }
 
     @Test
@@ -47,24 +47,24 @@ public class TestMaximumNumber {
     @Test
     public void givenMaxFloatNumber_AtThirdPosition_ShouldReturnSameNumber() {
         float largest = max.largest(2.0f, 1.0f, 3.0f);
-        Assert.assertEquals(3.0f,largest,0.0f);
+        Assert.assertEquals(3.0f, largest, 0.0f);
     }
 
     @Test
     public void givenMaxString_AtFirstPosition_ShouldReturnSameString() {
-        String largest = max.largest("Peach","Banana","Apple");
-        Assert.assertEquals("Peach",largest);
+        String largest = max.largest("Peach", "Banana", "Apple");
+        Assert.assertEquals("Peach", largest);
     }
 
     @Test
     public void givenMaxString_AtSecondPosition_ShouldReturnSameString() {
-        String largest = max.largest("Banana","Peach","Apple");
-        Assert.assertEquals("Peach",largest);
+        String largest = max.largest("Banana", "Peach", "Apple");
+        Assert.assertEquals("Peach", largest);
     }
 
     @Test
     public void givenMaxString_AtThirdPosition_ShouldReturnSameString() {
-        String largest = max.largest("Banana","Apple","Peach");
-        Assert.assertEquals("Peach",largest);
+        String largest = max.largest("Banana", "Apple", "Peach");
+        Assert.assertEquals("Peach", largest);
     }
 }
